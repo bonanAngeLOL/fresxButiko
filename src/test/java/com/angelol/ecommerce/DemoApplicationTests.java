30f96cf3cb1f06d590ce16e5ee84a08c9c1c6625
bonanAngeLOL <angellopezcue@gmail.com>
2021-02-16 02:04:26 -0600

    Init commit

---
+++ src/test/java/com/angelol/ecommerce/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angelol.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
