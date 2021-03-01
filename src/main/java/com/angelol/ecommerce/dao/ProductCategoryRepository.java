bbaadc428c898c708e720415ca181ebd6f41f233
bonanAngeLOL <angellopezcue@gmail.com>
2021-03-01 02:12:24 -0600

    Search products by name containing string

--- src/main/java/com/angelol/ecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/com/angelol/ecommerce/dao/ProductCategoryRepository.java
@@ -6,9 +6,11 @@ import com.angelol.ecommerce.entities.ProductCategory;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Transactional
 @RepositoryRestResource
+@CrossOrigin
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long>{
     
 
